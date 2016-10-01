389e5c9d5d67461cf1be0bff3b0ae718baf52497
Simeon Nenov <nenov.simeon@gmail.com>
2016-10-01 23:20:26 +0300

    #2 Wrote an integration test for HrefJavaScriptSanitizer

--- Html2Amp.IntegrationTests/Tests/HtmlToAmpConverting.cs
+++ Html2Amp.IntegrationTests/Tests/HtmlToAmpConverting.cs
@@ -19,5 +19,23 @@ namespace Html2Amp.IntegrationTests
 			// Assert
 			Assert.AreEqual("<amp-img src=\"test-image.png\" width=\"100\" height=\"100\" layout=\"responsive\"></amp-img>", ampHtml);
 		}
+
+		/// <summary>
+		/// This test is required, because of problem with relative urls in AngleSharp
+		/// and it's ensuring the output html is correct.
+		/// </summary>
+		[TestMethod]
+		public void ConvertingAnchorElementWithJavaScirptInHrefAttribute()
+		{
+			// Arrange
+			string htmlToConvert = "<a href=\"javascript:void(0);\">link</a>";
+			var htmlToAmpConverter = new HtmlToAmpConverter();
+
+			// Act
+			string ampHtml = htmlToAmpConverter.ConvertFromHtml(htmlToConvert);
+
+			// Assert
+			Assert.AreEqual("<a href=\"#\">link</a>", ampHtml);
+		}
 	}
 }
