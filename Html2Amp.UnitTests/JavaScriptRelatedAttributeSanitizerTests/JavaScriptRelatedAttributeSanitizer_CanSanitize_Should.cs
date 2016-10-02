fd206479f198ae7c42b99a636e8107bae2b8c3d0
izabelk <izi_girl@abv.bg>
2016-10-02 22:10:35 +0300

    #3 Implemented new JavaScriptRelatedAttributeSanitizer which role is to remove attributes starting with 'on' prefix. Added unit tests that cover the logic implemented in the sanitizer.

---
+++ Html2Amp.UnitTests/JavaScriptRelatedAttributeSanitizerTests/JavaScriptRelatedAttributeSanitizer_CanSanitize_Should.cs
@@ -0,0 +1,58 @@
+﻿using Html2Amp.Sanitization.Implementation;
+using Html2Amp.UnitTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Html2Amp.UnitTests.JavaScriptRelatedAttributeSanitizerTests
+{
+    [TestClass]
+    public class JavaScriptRelatedAttributeSanitizer_CanSanitize_Should
+    {
+        [TestMethod]
+        public void ReturnFalse_WhenTheHtmlElementIsNull()
+        {
+            // Act
+            var actualResult = new JavaScriptRelatedAttributeSanitizer().CanSanitize(null);
+
+            // Assert
+            Assert.IsFalse(actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_WhenTheHtmlElementHasNoAttributeStartingWithOnPrefix()
+        {
+            // Act
+            var actualResult = new JavaScriptRelatedAttributeSanitizer().CanSanitize(ElementFactory.CreateAnchor());
+
+            // Assert
+            Assert.IsFalse(actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_WhenTheHtmlElementHasAnAttributeStartingWithOnAndItsLengthIsGreaterThanTwo()
+        {
+            // Arrange
+            var htmlString = "<button onclick=\"myFunction()\">Click me</button>";
+            var htmlElement = ElementFactory.CreateFromHtmlString(htmlString);
+
+            // Act
+            var actualResult = new JavaScriptRelatedAttributeSanitizer().CanSanitize(htmlElement);
+
+            // Assert
+            Assert.IsTrue(actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_WhenTheHtmlElementHasAnAttributeWhichNameIsOn()
+        {
+            // Arrange
+            var htmlString = "<button on=\"true\">Click me</button>";
+            var htmlElement = ElementFactory.CreateFromHtmlString(htmlString);
+
+            // Act
+            var actualResult = new JavaScriptRelatedAttributeSanitizer().CanSanitize(htmlElement);
+
+            // Assert
+            Assert.IsFalse(actualResult);
+        }
+    }
+}
---
+++ Html2Amp/Sanitization/Implementation/JavaScriptRelatedAttributeSanitizer.cs
@@ -0,0 +1,32 @@
+﻿using AngleSharp.Dom;
+using ComboRox.Core.Utilities.SimpleGuard;
+using System.Linq;
+
+namespace Html2Amp.Sanitization.Implementation
+{
+    public class JavaScriptRelatedAttributeSanitizer : Sanitizer
+    {
+        public override bool CanSanitize(IElement element)
+        {
+            return element != null
+                && element.Attributes.Any(a => a.Name.StartsWith("on") 
+                    && a.Name.Length > 2);
+        }
+
+        public override IElement Sanitize(IDocument document, IElement htmlElement)
+        {
+            Guard.Requires(htmlElement, "htmlElement").IsNotNull();
+
+            var javascriptAttributes = htmlElement.Attributes
+                .Where(a => a.Name.StartsWith("on") && a.Name.Length > 2)
+                .ToList();
+
+            foreach (var attribute in javascriptAttributes)
+            {
+                htmlElement.RemoveAttribute(attribute.Name);
+            }
+
+            return htmlElement;
+        }
+    }
+}
