6c4ff79a1a9ec504afedbb75f85142cfa8d6392f
izabelk <izi_girl@abv.bg>
2016-10-22 11:14:20 +0300

    #19 Added integration tests for ScriptElementSanitizer.

---
+++ Html2Amp.IntegrationTests/Tests/ScriptElementSanitizationTests/ScriptElementSanitization.cs
@@ -0,0 +1,37 @@
+﻿using Html2Amp.IntegrationTests.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Html2Amp.IntegrationTests.Tests.ScriptElementSanitizationTests
+{
+	[TestClass]
+	public class ScriptElementSanitization
+	{
+		[TestMethod]
+		public void ScriptElementSanitizationWithOneScriptTag()
+		{
+			// Arrange
+			const string TestName = "ScriptElementSanitizationWithOneScriptTag";
+
+			// Act
+			var actualResult = HtmlTestFileToAmpConverter.Convert(TestName);
+
+			// Assert
+			HtmlAssert.AreEqual(TestDataProvider.GetOutFile(TestName), actualResult);
+			AmpAssert.IsValidAmp(TestName);
+		}
+
+		[TestMethod]
+		public void ScriptElementSanitizationWithMoreScriptTags()
+		{
+			// Arrange
+			const string TestName = "ScriptElementSanitizationWithMoreScriptTags";
+
+			// Act
+			var actualResult = HtmlTestFileToAmpConverter.Convert(TestName);
+
+			// Assert
+			HtmlAssert.AreEqual(TestDataProvider.GetOutFile(TestName), actualResult);
+			AmpAssert.IsValidAmp(TestName);
+		}
+	}
+}
