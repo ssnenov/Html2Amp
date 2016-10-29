9c5c55477b49e8faa62ed7ef4ef2b654a0109483
Simeon Nenov <nenov.simeon@gmail.com>
2016-10-29 15:27:17 +0300

    #17 Wrote integration tests for JavaScriptRelatedAttributeSanitizer

---
+++ Html2Amp.IntegrationTests/Tests/JavaScriptRelatedAttributeSanitizerTests/JavaScriptRelatedAttributeSanitizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Html2Amp.IntegrationTests.Infrastructure;
+
+namespace Html2Amp.IntegrationTests.Tests.JavaScriptRelatedAttributeSanitizerTests
+{
+	[TestClass]
+	public class JavaScriptRelatedAttributeSanitizer
+	{
+		[TestMethod]
+		public void JavaScriptRelatedAttributeSanitization()
+		{
+			// Arrange
+			const string TestName = "JavaScriptRelatedAttributeSanitization";
+
+			// Act
+			var actualResult = HtmlTestFileToAmpConverter.Convert(TestName);
+
+			// Assert
+			HtmlAssert.AreEqual(TestDataProvider.GetOutFile(TestName), actualResult);
+			AmpAssert.IsValidAmp(TestName);
+		}
+	}
+}
