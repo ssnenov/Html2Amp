77dcfd62515ea9e00b35911117a7e0eef94cc45b
Simeon Nenov <nenov.simeon@gmail.com>
2016-10-29 18:06:11 +0300

    Wrote documentation comments in RunConfiguration

--- Html2Amp/RunConfiguration.cs
+++ Html2Amp/RunConfiguration.cs
@@ -2,8 +2,20 @@
 {
 	public class RunConfiguration
 	{
+		/// <summary>
+		/// Gets or sets the host used for resolving relative URLs.
+		/// </summary>
+		/// <example>http://localhost</example>
+		/// <example>https://mydomain.com</example>
+		/// <example>mydomain.com</example>
+		/// <example>mydomain.com/blogs</example>
 		public string RelativeUrlsHost { get; set; }
 
+		/// <summary>
+		/// Gets or sets a flag used to determine shoud to download images. Downloading images
+		/// is used to set width and/or height of an image (if they aren't specified) in order to 
+		/// present the image responsively.
+		/// </summary>
         public bool ShouldDownloadImages { get; set; }
 
 		public RunConfiguration()
