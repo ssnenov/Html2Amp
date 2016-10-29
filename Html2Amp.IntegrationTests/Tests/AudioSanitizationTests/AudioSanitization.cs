c78fd77eb10fc9a9d1f83405ec88aca25a34328a
Simeon Nenov <nenov.simeon@gmail.com>
2016-10-29 16:32:39 +0300

    #17 Wrote integration tests for AudioSanitizer

---
+++ Html2Amp.IntegrationTests/Tests/AudioSanitizationTests/AudioSanitization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Html2Amp.IntegrationTests.Infrastructure;
+
+namespace Html2Amp.IntegrationTests.Tests.ImageSanitizerTests
+{
+	[TestClass]
+	public class AudioSanitization
+	{
+		[TestMethod]
+		public void AudioSanitizationWithControls()
+		{
+			// Arrange
+			const string TestName = "AudioSanitizationWithControls";
+
+			// Act
+			var actualResult = HtmlTestFileToAmpConverter.Convert(TestName);
+
+			// Assert
+			HtmlAssert.AreEqual(TestDataProvider.GetOutFile(TestName), actualResult);
+			AmpAssert.IsValidAmp(TestName);
+		}
+	}
+}
