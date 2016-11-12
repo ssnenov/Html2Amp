d7f7869f17b765b42244fa09037282602f50dadc
Izabel Gandileva <izi_girl@abv.bg>
2016-11-12 16:53:19 +0200

    #23 Added unit tests for ForbiddenAttributesSanitizer.

---
+++ Html2Amp.UnitTests/ForbiddenAttributesSanitizerTests/ForbiddenAttributesSanitizer_CanSanitize_Should.cs
@@ -0,0 +1,47 @@
+﻿using Html2Amp.Sanitization.Implementation;
+using Html2Amp.UnitTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Html2Amp.UnitTests.ForbiddenAttributesSanitizerTests
+{
+	[TestClass]
+	public class ForbiddenAttributesSanitizer_CanSanitize_Should
+	{
+		[TestMethod]
+		public void ReturnFalse_WhenElementArgumentIsNull()
+		{
+			// Act
+			var actualResult = new ForbiddenAttributesSanitizer().CanSanitize(null);
+
+			// Assert
+			Assert.IsFalse(actualResult);
+		}
+
+		[TestMethod]
+		public void ReturnFalse_WhenTheElementHasNoForbiddenAttributes()
+		{
+			// Arange
+			var element = ElementFactory.Create("div");
+
+			// Act
+			var actualResult = new ForbiddenAttributesSanitizer().CanSanitize(element);
+
+			// Assert
+			Assert.IsFalse(actualResult);
+		}
+
+		[TestMethod]
+		public void ReturnTrue_WhenTheElementHasForbiddenAttributes()
+		{
+			// Arange
+			var element = ElementFactory.Create("div");
+			element.SetAttribute("align", "center");
+
+			// Act
+			var actualResult = new ForbiddenAttributesSanitizer().CanSanitize(element);
+
+			// Assert
+			Assert.IsTrue(actualResult);
+		}
+	}
+}
---
+++ Html2Amp.UnitTests/ForbiddenAttributesSanitizerTests/ForbiddenAttributesSanitizer_Sanitize_Should.cs
@@ -0,0 +1,31 @@
+﻿using Html2Amp.Sanitization.Implementation;
+using Html2Amp.UnitTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Html2Amp.UnitTests.ForbiddenAttributesSanitizerTests
+{
+	[TestClass]
+	public class ForbiddenAttributesSanitizer_Sanitize_Should
+	{
+		[TestMethod]
+		public void ThorwArgumentNullException_WhenTheElementIsNull()
+		{
+			// Assert
+			Ensure.ArgumentExceptionIsThrown(() => new ForbiddenAttributesSanitizer().Sanitize(null, null), "htmlElement");
+		}
+
+		[TestMethod]
+		public void RemoveForbiddenAttribute_Always()
+		{
+			// Arange
+			var element = ElementFactory.Create("div");
+			element.SetAttribute("frameborder", "0");
+
+			// Act
+			var actualResult = new ForbiddenAttributesSanitizer().Sanitize(ElementFactory.Document, element);
+
+			// Assert
+			Assert.IsFalse(actualResult.HasAttribute("frameborder"));
+		}
+	}
+}
