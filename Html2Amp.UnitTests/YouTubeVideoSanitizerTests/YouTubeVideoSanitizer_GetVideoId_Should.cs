9d6c5bb270ddba2f7ea4da7e2a8d9b0ad0db1842
Simeon Nenov <nenov.simeon@gmail.com>
2016-11-25 23:23:45 +0200

    Wrote unit tests for YouTubeVideoSanitizer.GetVideoId() method

--- Html2Amp.UnitTests/YouTubeVideoSanitizerTests/YouTubeVideoSanitizer_GetVideoId_Should.cs
+++ Html2Amp.UnitTests/YouTubeVideoSanitizerTests/YouTubeVideoSanitizer_GetVideoId_Should.cs
@@ -14,5 +14,61 @@ namespace Html2Amp.UnitTests.YouTubeVideoSanitizerTests
 			// Assert
 			Ensure.ArgumentExceptionIsThrown(() => new YouTubeVideoSanitizerAccessor().GetVideoId(null), "videoUri");
 		}
+
+		[TestMethod]
+		public void ReturnVideoId_WhenUrlIsPassed()
+		{
+			// Arrange
+			const string ExpectedResult = "AuA9440gjt";
+			var uri = new Uri(string.Format("http://youtube.com/embed/{0}", ExpectedResult));
+
+			// Act
+			var actualResult = new YouTubeVideoSanitizerAccessor().GetVideoId(uri);
+
+			// Assert
+			Assert.AreEqual(ExpectedResult, actualResult);
+		}
+
+		[TestMethod]
+		public void ReturnVideoId_WhenUrlHasTrailingSlash()
+		{
+			// Arrange
+			const string ExpectedResult = "AuA9440gjt";
+			var uri = new Uri(string.Format("http://youtube.com/embed/{0}/", ExpectedResult));
+
+			// Act
+			var actualResult = new YouTubeVideoSanitizerAccessor().GetVideoId(uri);
+
+			// Assert
+			Assert.AreEqual(ExpectedResult, actualResult);
+		}
+
+		[TestMethod]
+		public void ReturnVideoId_WhenUrlHasQueryString()
+		{
+			// Arrange
+			const string ExpectedResult = "AuA9440gjt";
+			var uri = new Uri(string.Format("http://youtube.com/embed/{0}?q=1", ExpectedResult));
+
+			// Act
+			var actualResult = new YouTubeVideoSanitizerAccessor().GetVideoId(uri);
+
+			// Assert
+			Assert.AreEqual(ExpectedResult, actualResult);
+		}
+
+		[TestMethod]
+		public void ReturnVideoId_WhenUrlHasMorePaths()
+		{
+			// Arrange
+			const string ExpectedResult = "AuA9440gjt";
+			var uri = new Uri(string.Format("http://youtube.com/embed/{0}/test/", ExpectedResult));
+
+			// Act
+			var actualResult = new YouTubeVideoSanitizerAccessor().GetVideoId(uri);
+
+			// Assert
+			Assert.AreEqual(ExpectedResult, actualResult);
+		}
 	}
 }
